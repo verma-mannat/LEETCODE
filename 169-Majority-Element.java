9bc008a6e68ecf73d7e6deec3acb1da28823ba7a
verma-mannat <vmannat2007@gmail.com>
2026-07-03 22:49:49 +0530

    [169] [Time Beats: 69.93%] [Memory Beats: 17.66%] - LeetPush

--- 169-Majority-Element.java
+++ 169-Majority-Element.java
@@ -1,24 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int a=nums[0];
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                a=num;
             }
-            else{
-                map.put(nums[i],1);
+            if(num==a){
+                count++;
             }
+            else count--;
+            
         }
-        int majorityE=0;
-        int maxValue=0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > maxValue) {
-                maxValue = entry.getValue();
-                majorityE = entry.getKey();
-            }
-        }
-        return majorityE;
+        return a;
     }
 }
