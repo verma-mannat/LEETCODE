b402d63c562ad7255178b8e046a7f929fd536069
verma-mannat <vmannat2007@gmail.com>
2026-07-04 22:42:53 +0530

    [53] [Time Beats: 99.96%] [Memory Beats: 48.02%] - LeetPush

--- 53-Maximum-Subarray.java
+++ 53-Maximum-Subarray.java
@@ -4,8 +4,18 @@ class Solution {
         int maxSum = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(nums[i], sum + nums[i]);
-            maxSum = Math.max(maxSum, sum);
+
+            // Kadane's Algorithm
+            if (sum < 0) {
+                sum = nums[i];      // Start a new subarray
+            } else {
+                sum += nums[i];     // Continue the current subarray
+            }
+
+            // Update maximum sum found so far
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
         }
 
         return maxSum;
