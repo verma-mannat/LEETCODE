fd027bd0de4c1cee4fbb57b8b301c7880750d8f7
verma-mannat <vmannat2007@gmail.com>
2026-07-04 23:08:13 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 22.27%] - LeetPush

---
+++ 50-Pow(x,-n).java
@@ -0,0 +1,28 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return pow(x, N);
+    }
+
+    private double pow(double x, long n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = pow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        }//sala 
+
+        return half * half * x;    
+    }
+}
