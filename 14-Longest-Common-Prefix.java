fc965fe8e17553d17813218e1eb5a9ba661f4ac3
verma-mannat <vmannat2007@gmail.com>
2026-07-06 00:06:57 +0530

    [14] [Time Beats: 62.66%] [Memory Beats: 32.71%] - LeetPush

---
+++ 14-Longest-Common-Prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0){
+            return "";
+        }
+        Arrays.sort(strs);
+        String first=strs[0];//ye bass 1st or last why dekh?//sort kiya na to baki sb bichme aa gye first and last sbse bada and chota //oki
+        String last=strs[strs.length -1];
+        int i=0;
+        while(i<first.length() && i<last.length() && 
+                first.charAt(i)==last.charAt(i)){
+                i++;
+            }
+        return first.substring(0,i);
+    }
+    
+}
