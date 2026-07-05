6d727fa1b6a3b06a3660201f5908e6e5b5ed8fab
verma-mannat <vmannat2007@gmail.com>
2026-07-06 01:34:12 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 50.51%] - LeetPush

--- 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -1,18 +1,45 @@
-
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int[] result = {-1, -1};
-        
-        for(int i = 0; i < n; i++){
-            if(nums[i] == target){
-                if(result[0] == -1){ 
-                    result[0] = i;
-                }
-                result[1] = i; 
+        return new int[]{first(nums, target), last(nums, target)};
+    }
+
+    public int first(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;   // Search on left side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        
-        return result;
+
+        return ans;
+    }
+
+    public int last(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;    // Search on right side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
     }
 }
