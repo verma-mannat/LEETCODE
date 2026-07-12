a871f439340f5e3e9021b061bc64929b3dbffe85
verma-mannat <vmannat2007@gmail.com>
2026-07-13 02:25:20 +0530

    [643] [Time Beats: 32.74%] [Memory Beats: 80.22%] - LeetPush

--- 643-Maximum-Average-Subarray-I.java
+++ 643-Maximum-Average-Subarray-I.java
@@ -5,14 +5,11 @@ class Solution {
         for(int i=0;i<k;i++){
             sum=sum+nums[i];//sum
         }
-        max=sum;//max =sum/n !? y k?//are pehle ek slide then baki slide ki compare//ok
-        double m2=0;
+        max=sum;
         for(int i=k;i<nums.length;i++){
-            sum=sum+nums[i]-nums[i-k];//dekh to shi hai?//ig i=k se nhi hona chhaiye!?//yha
-            //dekhte hai
-            max=Math.max(sum,max);//sb shi to lg rha
+            sum=sum+nums[i]-nums[i-k];
+            max=Math.max(sum,max);
         }
         return max/k;
-    }//mistake dundhni hogi//kiya kya thoda explain
-}//1 2 3 4 5 k=2
-//max=1.5
+    }
+}
