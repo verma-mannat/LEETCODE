266dfb6d850bf15c77074ae8c67bfced407a8ab9
verma-mannat <vmannat2007@gmail.com>
2026-07-14 21:48:35 +0530

    [744] [Time Beats: 100.00%] [Memory Beats: 96.79%] - LeetPush

---
+++ 744-Find-Smallest-Letter-Greater-Than-Target.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+        int n=letters.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid=left+ (right-left)/2;
+            if(letters[mid]<=target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        if (left < letters.length) {
+            return letters[left];
+        } else {
+            return letters[0];
+        }
+    }
+}
