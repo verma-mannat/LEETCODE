f7b34a5a54ff72f448546c008b54131069e4aa88
verma-mannat <vmannat2007@gmail.com>
2026-07-21 23:55:24 +0530

    [1436] [Time Beats: 99.83%] [Memory Beats: 5.83%] - LeetPush

--- 1436-Destination-City.java
+++ 1436-Destination-City.java
@@ -1,34 +1,16 @@
-/* class Solution {
-    public String destCity(List<List<String>> paths) {
-        HashSet<String> in = new HashSet<>();
-        HashSet<String> out = new HashSet<>();
-
-        for (List<String> path : paths) {
-            in.add(path.get(0));
-            out.add(path.get(1));
-        }
-        for (String city : in) {
-            out.remove(city);
-        }
-        //set print krwa de return in krke dekh//return in .element how?//return krna hog
-        return out.iterator().next();
-        //ohh 
-    }//ohh konsa
-}//iska ek simple version mila hai */
 class Solution {
     public String destCity(List<List<String>> paths) {
-        HashSet<String> out = new HashSet<>();
+        Map<String, String> map = new HashMap<>();
 
-        for (List<String> path : paths) {
-            out.add(path.get(0));
+        for (List<String> pair : paths) {
+            map.put(pair.get(0), pair.get(1));
         }
 
-        for (List<String> path : paths) {
-            if (!out.contains(path.get(1))) {
-                return path.get(1);
+        for (String value : map.values()) {
+            if (!map.containsKey(value)) {
+                return value; // directly return destination city
             }
         }
-
         return "";
     }
 }
