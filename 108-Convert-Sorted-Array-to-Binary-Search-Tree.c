a16eaa63f0b1e5f684abc554ca8cf701772b5c88
verma-mannat <vmannat2007@gmail.com>
2026-08-23 21:24:01 +0530

    [108] [Time Beats: 68.19%] [Memory Beats: 36.43%] - LeetPush

---
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree.c
@@ -0,0 +1,17 @@
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    if (numsSize == 0)
+        return NULL;
+
+    int mid = numsSize / 2;
+
+    struct TreeNode* root = malloc(sizeof(struct TreeNode));
+
+    root->val = nums[mid];
+
+    root->left = sortedArrayToBST(nums, mid);
+
+    root->right = sortedArrayToBST(nums + mid + 1,
+                                   numsSize - mid - 1);
+
+    return root;
+}
