1bf3ba0e94ac4ddf52f3e43fb87318dd6d9deb0d
verma-mannat <vmannat2007@gmail.com>
2026-09-03 21:44:41 +0530

    [3876] [Time Beats: 100.00%] [Memory Beats: 74.64%] - LeetPush

---
+++ DCP-09-26/3876-Construct-Uniform-Parity-Array-II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+        }
+
+        // Smallest number is odd
+        if (min % 2 == 1) {
+            return true;
+        }
+
+        // Smallest number is even
+        // Then all numbers must be even
+        for (int num : nums1) {
+            if (num % 2 == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
