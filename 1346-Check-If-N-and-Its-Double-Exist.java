294eca2aed9feb3444acfd3fb25a8a667628b3a2
verma-mannat <vmannat2007@gmail.com>
2026-09-11 21:12:11 +0530

    [1346] [Time Beats: 96.81%] [Memory Beats: 95.04%] - LeetPush

---
+++ 1346-Check-If-N-and-Its-Double-Exist.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i]==2*arr[j] && i!=j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
