a2b98180bd0cf794f4a7dd0c403ab816c8b805ca
verma-mannat <vmannat2007@gmail.com>
2026-09-14 23:43:23 +0530

    [3945] [Time Beats: 98.44%] [Memory Beats: 24.62%] - LeetPush

--- 3945-Digit-Frequency-Score.java
+++ 3945-Digit-Frequency-Score.java
@@ -1,22 +1,11 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        //hashmap bnate hai//yes
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        int p=n;
-        int sum=0;
-        while(p>0){//sab digit map mei add hogi freq map jaise
-            int digit=p%10;
-            map.put(digit,map.getOrDefault(digit,0)+1);
-            p /= 10;
-        }
-        int result=0;
-       
-        for (int key : map.keySet()) {
-            int value = map.get(key);
-            result += key * value;
-        }
-        return result;
+        int temp=n;
+        int sum =0;
+        while(temp>0){
+            int digit = temp%10;
+            sum += digit;
+            temp/=10;
+        }return sum;
     }
 }
